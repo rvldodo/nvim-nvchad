23f6ed402eed7d120f7b30e4b4856a1ccfc1b72e
Hamdani Abdullah <hamdaniadullah961106@gmail.com>
2024-04-20 11:38:31 +0700

    add gomodifytags plugins

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -126,5 +126,16 @@ local plugins = {
       return require "custom.configs.null-ls"
     end,
   },
+  {
+    'simondrake/gomodifytags',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    vim.api.nvim_create_user_command('GoAddTags', function(opts) require('gomodifytags').GoAddTags(opts.fargs[1], opts.args) end, { nargs = "+" }),
+    opts = {
+      transformation = "snakecase",
+      skip_unexported = true,
+      override = true,
+      options = { "json=omitempty" }
+    },
+  }
 }
 return plugins
